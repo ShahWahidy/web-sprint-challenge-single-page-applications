9da5c0561d2833f66d2056234979e793aa073286
Ahmadshah Wahidy <shah.wahidy@gmail.com>
2021-12-17 00:08:37 -0800

    inital setup of the sprint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { Switch, Route, Link, useHistory } from "react-router-dom"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Home from "./components/Home";
+import Order from "./components/Order";
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <nav>
+        <h1 className='store-header'>Bloom Tech Pizza Day</h1>
+        <div className='nav-links'>
+          <Link to="/">Home</Link>
+          <Link to="/order-pizza">Order</Link>
+        </div>
+      </nav>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='order-pizza'>
+          <Order />
+        </Route>
+
+      </Switch>
+
     </>
   );
 };
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Home() {
+  
+  return (
+    <div className='home-wrapper'>
+      <img
+        className='home-image'
+        src='https://source.unsplash.com/IfQlwNqJqV8'
+        alt=''
+      />
+     
+    </div>
+  )
+}
